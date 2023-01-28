39f7b40cda87e64d34a0905d04186d0533fcbb92
davidnene <davienesh4@gmail.com>
2023-01-28 18:32:32 +0300

    Add resources

--- api.py
+++ api.py
@@ -1,2 +1,15 @@
 from flask import Flask
 from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'data': 'Hello, world!'}
+
+api.add_resource(HelloWorld, '/helloworld')
+#api.add_resource(HelloName, '/helloworld/<string:name>')
+
+if __name__ == "__main__":
+    app.run(debug=True)
